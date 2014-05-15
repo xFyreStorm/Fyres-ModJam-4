a34fe07dbdd98f2fc632c40e6227b0672b10451f
Kenneth Dignam <kennethdignam@hotmail.com>
2014-05-14 21:48:35 -0400

    Working on more stats for base weapon class.

--- java/fyresmodjam4/handlers/CommonTickHandler.java
+++ java/fyresmodjam4/handlers/CommonTickHandler.java
@@ -1,9 +1,36 @@
 package fyresmodjam4.handlers;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldServer;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.ServerTickEvent;
+import fyresmodjam4.items.ItemWeapon;
 
 public class CommonTickHandler {
 	@SubscribeEvent
-	public void serverTick(ServerTickEvent event) {}
+	public void serverTick(ServerTickEvent event) {
+		MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
+		
+		if(server != null) {
+			for(int i = 0; i < server.worldServers.length; i++) {
+				WorldServer s = server.worldServers[i];
+				
+				if(s == null) {continue;}
+				
+				for(Object o : s.playerEntities) {
+					if(o == null || !(o instanceof EntityPlayer)) {continue;}
+					
+					EntityPlayer player = (EntityPlayer) o;
+					
+					//Inefficient :P Need to find a better place to init stacks.
+					for(ItemStack stack : player.inventory.mainInventory) {
+						if(stack != null && stack.getItem() instanceof ItemWeapon) {ItemWeapon.giveStats(stack);}
+					}
+				}
+			}
+		}
+	}
 }
--- java/fyresmodjam4/items/ItemWeapon.java
+++ java/fyresmodjam4/items/ItemWeapon.java
@@ -2,6 +2,9 @@ package fyresmodjam4.items;
 
 import fyresmodjam4.Modjam4;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -16,6 +19,7 @@ public class ItemWeapon extends Item {
 	
 	public ItemWeapon(String name) {
 		this.name = name;
+		this.setMaxStackSize(1);
 	}
 	
 	@Override
@@ -33,22 +37,22 @@ public class ItemWeapon extends Item {
 	
 	@Override
 	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
-		giveStats(itemStack);
-		
 		if(!world.isRemote) {
 			if(itemStack.hasTagCompound()) {
 				boolean shouldFire = false;
 				
+				//TODO Check player ammo first.
+				
 				NBTTagCompound tagCompound = itemStack.getTagCompound();
 				
 				long time = System.currentTimeMillis();
-				
 				if(time - tagCompound.getLong("lastShot") >= tagCompound.getInteger("shotDelay")) {
 					tagCompound.setLong("lastShot", time);
 					shouldFire = true;
 				}
 				
 				if(shouldFire) {
+					Entity entity = EntityList.createEntityByID(tagCompound.getInteger("firedEntity"), world);
 					world.playSoundAtEntity(player, "fyresmodjam4:bullet_shot", 1.0F, 1.0F);
 				}
 			}
@@ -59,15 +63,17 @@ public class ItemWeapon extends Item {
 		return itemStack;
 	}
 
-	public void giveStats(ItemStack stack) {
+	public static void giveStats(ItemStack stack) {
 		if(!stack.hasTagCompound()) {stack.setTagCompound(new NBTTagCompound());}
 		NBTTagCompound tagCompound = stack.getTagCompound();
 		
 		if(tagCompound != null && !tagCompound.hasKey("givenStats")) {
 			tagCompound.setBoolean("givenStats", true);
 			
-			tagCompound.setInteger("shotDelay", 200);
+			tagCompound.setInteger("shotDelay", 200); //Randomized, or preset?
 			tagCompound.setLong("lastShot", (int) System.currentTimeMillis());
+			
+			tagCompound.setInteger("firedEntity", EntityList.getEntityID(new EntityCow(null)));
 		}
 	}
 }
